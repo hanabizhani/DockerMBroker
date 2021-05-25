7ca3608c388254a51513cb2d77ff05e3bc22f7be
Hana Bizhani <hanabizhani@gmail.com>
2021-05-26 01:36:26 +0430

    Create Message.java

---
+++ src/main/java/domain/Message.java
@@ -0,0 +1,12 @@
+public class Message{
+       private String myMessage;
+      
+       public void setMyMessage(String myMessage){
+           this.myMessage = myMessage;
+       }
+
+
+       public String getMyMessage(){
+           return myMessage;
+       }
+}
